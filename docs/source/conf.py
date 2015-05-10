22772e96262df1ccf21e76eb84720f012335a27c
Wolfram Eberius <edrush@posteo.de>
2015-05-10 11:04:22 +0200

    Configured sphinx extensions path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,13 +16,10 @@ import sys
 import os
 import shlex
 
-# add local extensions path to include path
-sys.path.append('extensions')
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../extensions'))
 
 # -- General configuration ------------------------------------------------
 
