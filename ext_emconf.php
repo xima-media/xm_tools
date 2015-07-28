751e63e82101e3d75fd9593fd01229b80410183b
Wolfram Eberius <edrush@posteo.de>
2015-07-28 11:46:04 +0200

    Updated composer.json and TYPO3 dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,9 +30,9 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '0.2.0',
     'constraints' => array(
         'depends' => array(
-            'extbase' => '6.2',
-            'fluid' => '6.2',
-            'typo3' => '6.2',
+            'typo3' => '6.2.0-6.2.99',
+            'extbase' => '6.2.0-',
+            'fluid' => '6.2.0-',
         ),
         'conflicts' => array(
         ),
