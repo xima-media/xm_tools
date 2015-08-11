2550d1232ab747018bae5282f3e9c8ca95b369c6
Steve Lenz <steve.lenz@xima.de>
2015-08-11 14:57:42 +0200

    [Improvement] More functionality for FeUser

--- Classes/Typo3/FeUser.php
+++ Classes/Typo3/FeUser.php
@@ -3,12 +3,60 @@
 namespace Xima\XmTools\Classes\Typo3;
 
 /**
- * Helper for FeUser.
+ * Helper for FeUser
  *
- * @author Steve Lenz <sle@xima.de>, Sebastian Gierth <sgi@xima.de>
+ * @author Steve Lenz <kontakt@steve-lenz.de>, Sebastian Gierth <sgi@xima.de>
+ * @package TYPO3 6.2.x
+ * @version 1.2.0
  */
 class FeUser
 {
+    /**
+     * @var FeUser
+     */
+    private $user = NULL;
+
+    /**
+     * @var FeUser groupData
+     */
+    private $groupData = NULL;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        if ($GLOBALS['TSFE']->fe_user->user) {
+            $this->user = $GLOBALS['TSFE']->fe_user->user;
+            $this->groupData = $GLOBALS['TSFE']->fe_user->groupData;
+        }
+
+    }
+
+    /**
+     * @return int
+     */
+    public function getUid()
+    {
+        return $this->user['uid'];
+    }
+
+    /**
+     * @return array
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @return array
+     */
+    public function getGroupData()
+    {
+        return $this->groupData;
+    }
+
     /**
      * Checks if fe_user is authenticated.
      *
@@ -16,10 +64,9 @@ class FeUser
      */
     public function feUserIsAuthenticated()
     {
-        if (empty($GLOBALS['TSFE']->fe_user->user['uid'])) {
+        if (null == empty($this->getUid())) {
             return false;
         }
-
         return true;
     }
 
@@ -32,26 +79,25 @@ class FeUser
      */
     public function feUserHasRole($role)
     {
-        if ($this->feUserIsAuthenticated() && in_array($role, $GLOBALS['TSFE']->fe_user->groupData['title'])) {
+        if ($this->feUserIsAuthenticated() && in_array($role, $this->groupData['title'])) {
             return true;
         }
-
         return false;
     }
 
     /**
      * Checks if fe_user is authenticated and has given role id.
      *
-     * @param int $role
+     * @param int $roleId
      *
      * @return bool
      */
-    public function feUserHasRoleId($role)
+    public function feUserHasRoleId($roleId)
     {
-        if ($this->feUserIsAuthenticated() && in_array($role, $GLOBALS['TSFE']->fe_user->groupData['uid'])) {
+        if ($this->feUserIsAuthenticated() && in_array($roleId, $this->groupData['uid'])) {
             return true;
         }
-
         return false;
     }
+
 }
