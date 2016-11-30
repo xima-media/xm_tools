1780c48d94300885dd16e03db51fd8023567528a
Steve Lenz <sle@xima.de>
2016-11-30 11:19:23 +0100

    [Feature] Add FeUser

---
+++ Classes/Security/FeUser.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace Xima\XmTools\Security;
+
+/**
+ * Layer for the TYPO3 fe_user
+ *
+ * @author Steve Lenz <kontakt@steve-lenz.de>
+ * @copyright (c) 2015, Steve Lenz
+ */
+class FeUser
+{
+
+    /**
+     * Checks whether fe_user exists
+     *
+     * @return bool
+     */
+    public static function isFeUser()
+    {
+        return ($GLOBALS['TSFE']->fe_user->user) ? true : false;
+    }
+
+    /**
+     * Returns the parsed TSconfig for the fe_user
+     * The TSconfig will be cached in $this->userTS.
+     *
+     * @return array
+     */
+    public static function getUserTSConfig()
+    {
+        return $GLOBALS['TSFE']->fe_user->getUserTSconf();
+    }
+
+    /**
+     * Returns the fe_user uid
+     *
+     * @return int
+     */
+    public static function getUid()
+    {
+        return $GLOBALS['TSFE']->fe_user->user['uid'];
+    }
+
+    /**
+     * Returns fe_user data
+     *
+     * @param null $key - If null you get the whole user data array
+     * @return mixed - false if key not exists
+     */
+    public static function getUser($key = null)
+    {
+        if (null == $key) {
+            return $GLOBALS['TSFE']->fe_user->user;
+        } else {
+            return (isset($GLOBALS['TSFE']->fe_user->user[$key])) ? $GLOBALS['TSFE']->fe_user->user[$key] : false;
+        }
+
+    }
+
+    /**
+     * Returns the whole fe_user group data
+     *
+     * @return array
+     */
+    public static function getGroupData()
+    {
+        return $GLOBALS['TSFE']->fe_user->groupData;
+    }
+
+    /**
+     * Checks whether fe_user is authenticated
+     *
+     * @return bool
+     */
+    public static function isAuthenticated()
+    {
+        return (self::isFeUser() && null != self::getUid());
+    }
+
+    /**
+     * Checks whether fe_user is authenticated and has given role
+     *
+     * @param string $role
+     * @return bool
+     */
+    public static function hasRole($role)
+    {
+        return (self::isFeUser()
+            && self::isAuthenticated()
+            && in_array($role, $GLOBALS['TSFE']->fe_user->groupData['title'])
+        );
+    }
+
+    /**
+     * Checks whether fe_user is authenticated and has given role id
+     *
+     * @param int $role
+     * @return bool
+     */
+    public static function hasRoleId($role)
+    {
+        return (self::isFeUser()
+            && self::isAuthenticated()
+            && in_array($role, $GLOBALS['TSFE']->fe_user->groupData['uid'])
+        );
+    }
+
+}
