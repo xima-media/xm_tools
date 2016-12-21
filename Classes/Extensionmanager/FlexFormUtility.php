d0eacde27daad1e6f69d4333668eb8fe5e1a48e6
Markus Hackel <mha@xima.de>
2016-12-21 16:36:38 +0100

    [Feature] FlexFormUtility zum Mergen von FlexForm Definitionen

---
+++ Classes/Extensionmanager/FlexFormUtility.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: markus
+ * Date: 21.12.16
+ * Time: 12:21
+ */
+
+namespace Xima\XmTools\Extensionmanager;
+
+
+use TYPO3\CMS\Core\Configuration\FlexForm\FlexFormTools;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+class FlexFormUtility
+{
+    /*
+    * Extend existing FlexForm definition
+    *
+    * @param $base base-Extension-Name
+    * @param $extension Extension-Name
+    * @param $original filename of flexform
+    * @param $addition filename of additional flexform
+    *
+    */
+    public function mergeFlexFormDefinitions ($base, $extension, $original, $addition)
+    {
+        $old = GeneralUtility::getUrl(
+            ExtensionManagementUtility::extPath($base, 'Configuration/FlexForms/' . $original)
+        );
+        $currentValueArray = GeneralUtility::xml2array($old);
+
+        $add = GeneralUtility::getUrl(
+            ExtensionManagementUtility::extPath($extension, 'Configuration/FlexForms/' . $addition)
+        );
+        $additionalValueArray = GeneralUtility::xml2array($add);
+
+        $newValueArray = $currentValueArray;
+
+        foreach ($currentValueArray['sheets'] as $sheetTitle => $sheet) {
+            if (is_array($additionalValueArray)
+                && is_array($additionalValueArray['sheets'])
+                && isset($additionalValueArray['sheets'][$sheetTitle])) {
+                $newValueArray['sheets'][$sheetTitle]['ROOT']['el'] = array_merge(
+                    $currentValueArray['sheets'][$sheetTitle]['ROOT']['el'],
+                    $additionalValueArray['sheets'][$sheetTitle]['ROOT']['el']
+                );
+            }
+        }
+
+        $flexTools = GeneralUtility::makeInstance(FlexFormTools::class);
+        $new = $flexTools->flexArray2Xml($newValueArray, TRUE);
+
+        return $new;
+    }
+}
