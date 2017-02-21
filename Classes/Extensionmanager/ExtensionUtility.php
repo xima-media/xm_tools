8f3640067abab0dabbce37f151a6cfb56ebc8d8f
Steve Lenz <sle@xima.de>
2017-02-21 11:27:04 +0100

    [Feature] Methode zum auslesen der TypoScript Plugin Setup

--- Classes/Extensionmanager/ExtensionUtility.php
+++ Classes/Extensionmanager/ExtensionUtility.php
@@ -4,6 +4,7 @@ namespace Xima\XmTools\Extensionmanager;
 
 use TYPO3\CMS\Core\Package\Package;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
 use TYPO3\CMS\Extbase\Object\ObjectManager;
 use TYPO3\CMS\Extensionmanager\Utility\ListUtility;
 
@@ -11,6 +12,7 @@ use TYPO3\CMS\Extensionmanager\Utility\ListUtility;
 /**
  * Class ExtensionUtility
  *
+ * @author Steve Lenz <steve.lenz@xima.de>
  * @package Xima\XmTools\Extensionmanager
  */
 class ExtensionUtility
@@ -42,6 +44,49 @@ class ExtensionUtility
         }
 
         return $version;
+    }
+
+    /**
+     * Returns TypoScript plugin setup for given extension
+     *
+     * @param string $extKey
+     * @param string $pluginName
+     * @return array
+     */
+    public static function getTypoScriptPluginSetup($extKey, $pluginName = '')
+    {
+        $pluginKey = strtolower(str_replace('_', '', $extKey));
+        $pluginKey .= ($pluginName) ? '_' . strtolower($pluginName) : '';
+        $pluginKey = 'tx_' . $pluginKey . '.';
+
+        /** @var ObjectManager $om */
+        $om = GeneralUtility::makeInstance(ObjectManager::class);
+        /** @var ConfigurationManagerInterface $confMngr */
+        $confMngr = $om->get(ConfigurationManagerInterface::class);
+        $tsSetup = $confMngr->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
+
+        if (array_key_exists($pluginKey, $tsSetup['plugin.'])) {
+            return static::removeDots((array)$tsSetup['plugin.'][$pluginKey]);
+        } else {
+            return [];
+        }
+    }
+
+    /**
+     * Removes dots in array keys
+     *
+     * @param array $array
+     * @return array
+     */
+    private static function removeDots(array $array)
+    {
+        $conf = [];
+
+        foreach ($array as $key => $value) {
+            $conf[preg_replace('/\.$/', '', $key)] = is_array($value) ? static::removeDots($value) : $value;
+        }
+
+        return $conf;
     }
 
 }
