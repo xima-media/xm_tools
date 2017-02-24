9eb34b50435ae9ea480c2930dee85f0908e7f81e
Steve Lenz <sle@xima.de>
2017-02-24 14:13:06 +0100

    [Task] Enable support for TYPO3 6.2.30

--- ext_emconf.php
+++ ext_emconf.php
@@ -31,7 +31,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints'      => array(
         'depends'   => array(
             'php'   => '5.6.0-7.0.99',
-            'typo3' => '7.6.00-7.6.99',
+            'typo3' => '6.2.30-7.6.99',
         ),
         'conflicts' => array(),
         'suggests'  => array(),
