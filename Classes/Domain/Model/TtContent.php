8ab2c9cc69194ab720e3cd9719d2322c82eff909
Steve Lenz <sle@xima.de>
2018-05-07 15:59:09 +0200

    [Task] Add TtContent to Domain-Model

---
+++ Classes/Domain/Model/TtContent.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace Xima\XmTools\Domain\Model;
+
+use TYPO3\CMS\Extbase\DomainObject\AbstractEntity;
+
+/**
+ * Class TtContent
+ *
+ * Model of tt_content
+ *
+ * @package Xima\XmTools\Domain\Model
+ */
+class TtContent extends AbstractEntity
+{
+
+    /**
+     * uid
+     *
+     * @var string
+     */
+    protected $uid = '';
+
+    /**
+     * pid
+     *
+     * @var string
+     */
+    protected $pid = '';
+
+    /**
+     * header
+     *
+     * @var string
+     */
+    protected $header = '';
+
+    /**
+     * sorting
+     *
+     * @var string
+     */
+    protected $sorting = '';
+
+    /**
+     * contentType
+     *
+     * @var string
+     */
+    protected $contentType = '';
+
+    /**
+     * @var string
+     */
+    protected $piFlexform;
+
+    /**
+     * @var string
+     */
+    protected $listType;
+
+    /**
+     * Gets the uid
+     *
+     * @return string $uid
+     */
+    public function getUid()
+    {
+        return $this->uid;
+    }
+
+    /**
+     * Gets the pid
+     *
+     * @return string $pid
+     */
+    public function getPid()
+    {
+        return $this->pid;
+    }
+
+    /**
+     * Returns the header
+     *
+     * @return string $header
+     */
+    public function getHeader()
+    {
+        return $this->header;
+    }
+
+    /**
+     * Sets the header
+     *
+     * @param string $header
+     * @return void
+     */
+    public function setHeader($header)
+    {
+        $this->header = $header;
+    }
+
+    /**
+     * Returns the sorting
+     *
+     * @return string $sorting
+     */
+    public function getSorting()
+    {
+        return $this->sorting;
+    }
+
+    /**
+     * Sets the sorting
+     *
+     * @param string $sorting
+     * @return void
+     */
+    public function setSorting($sorting)
+    {
+        $this->sorting = $sorting;
+    }
+
+    /**
+     * Returns the contentType
+     *
+     * @return string $contentType
+     */
+    public function getContentType()
+    {
+        return $this->contentType;
+    }
+
+    /**
+     * Sets the contentType
+     *
+     * @param string $contentType
+     * @return void
+     */
+    public function setContentType($contentType)
+    {
+        $this->contentType = $contentType;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getPiFlexform()
+    {
+        return $this->piFlexform;
+    }
+
+    /**
+     * @return string
+     */
+    public function getListType()
+    {
+        return $this->listType;
+    }
+
+}
---
+++ Classes/Domain/Repository/TtContentRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Xima\XmTools\Domain\Model;
+
+use TYPO3\CMS\Extbase\Persistence\Repository;
+
+/**
+ * Class TtContentRepository
+ *
+ * @package Xima\XmTools\Domain\Model
+ */
+class TtContentRepository extends Repository
+{
+
+    /**
+     *
+     */
+    public function initializeObject()
+    {
+        /** @var  $querySettings */
+        $querySettings = $this->objectManager->get(Typo3QuerySettings::class);
+        $querySettings->setRespectStoragePage(false);
+        $this->setDefaultQuerySettings($querySettings);
+    }
+
+}
