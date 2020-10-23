4f8f1288e4a1c937ffa4ebac005217bf97186420
Konrad Michalik <konrad.michalik@xima.de>
2020-10-23 09:49:46 +0200

    [Task] Adding support for PHP 7.3 and 7.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -30,7 +30,7 @@ $EM_CONF[$_EXTKEY] = [
     'version'          => '9.5.10',
     'constraints'      => [
         'depends'   => [
-            'php'   => '7.2.0-7.2.99',
+            'php'   => '7.2.0-7.4.99',
             'typo3' => '9.5.0-9.5.99',
         ],
         'conflicts' => [],
