295902a295b0e9f852c87bc349776cfbfb9bf071
Markus Hackel <mha@xima.de>
2021-02-17 16:19:43 +0100

    [Improvement] Register global namespace "xmt" for ViewHelpers

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,3 +6,6 @@ if (TYPO3_MODE === 'BE') {
      */
     $GLOBALS['TYPO3_CONF_VARS']['BE']['toolbarItems'][1571038828] = \Xima\XmTools\Backend\ToolbarItems\WebsiteVersionToolbarItem::class;
 }
+
+// Register "xmt" as global fluid namespace
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['xmt'] = ['Xima\\XmTools\\ViewHelpers'];
